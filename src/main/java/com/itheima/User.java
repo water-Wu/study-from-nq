6cef4cf735ba3f526fd2e53996a2d8d1368c54ae
wlq <2307019575@qq.com>
2019-01-15 21:04:32 +0800

    第一天 ----年强老师

---
+++ src/main/java/com/itheima/User.java
@@ -0,0 +1,14 @@
+package com.itheima;
+
+import lombok.*;
+
+/**
+ * @author wuliqiang@itcast.cn
+ * time  2019/1/15
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private String username;
+}
